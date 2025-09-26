f0d868c4a51f1766a1516de1805e034ed1a087f0
Mizan310 <mizan310310@gmail.com>
2025-09-26 11:19:50 +0600

    Added ydata profiling except pandas profiling

--- main.py
+++ main.py
@@ -2,7 +2,14 @@
 # installed pandas-profiling
 
 import pandas as pd
-from pandas_profiling import ProfileReport 
+# from pandas_profiling import ProfileReport 
+from ydata_profiling import ProfileReport
+
 
 df = pd.read_csv('housing.csv')
 print(df)
+
+# Generate a report
+
+profile = ProfileReport(df)
+profile.to_file(output_file="housing.html")
